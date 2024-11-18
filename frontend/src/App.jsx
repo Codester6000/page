f0460d073e3bec767d2f5d9b1b303a065458b479
Ramon Barrera <rebcontacto@gmail.com>
2024-11-18 20:36:50 -0300

    Se creo el menu dezplegable y se hizo funcional el boton del menu en la navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,15 @@ import 'swiper/swiper-bundle.css';
 import "./styl.css"
 import Carousel from "./componentes/Carousel";
 import ProductCard from "./componentes/ProductCard";
+import MenuNavbar from "./componentes/MenuNavbar";
+
+// IMPORTANTE HACER 
+// npm install @mui/joy @emotion/react @emotion/styled
+// npm install swiper     
+// npm install @mui/lab   
+// npm install @mui/material @mui/styled-engine-sc styled-components
+// npm install @mui/icons-material
+
 
 function App() {
 
--- frontend/src/componentes/Navbar.jsx
+++ frontend/src/componentes/Navbar.jsx
@@ -1,21 +1,30 @@
-import { AppBar, Button, IconButton, colors } from "@mui/material";
+import { AppBar, Button, Drawer, IconButton, colors } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import CustomizedInputBase from "./CustomizedInputBase";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import PersonIcon from '@mui/icons-material/Person';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useState } from "react";
+import MenuNavbar from "./MenuNavbar";
 
 
 
 export default function Navbar() {
 
+    const [abierto , setAbierto] = useState(false)
+
     return (
         <>
             <AppBar position="static" style={{ background: "#FF7d21"}}>
                 <Toolbar>
-                    <MenuIcon size="large" edge="start" color="orange" aria-label="menu" sx={{ mr: 2 }} ></MenuIcon>
-                        <img src="./public/modex.png" alt="logo modex" style={{ width: "150px", height: "auto", padding: "15px", paddingRight: "10px" , filter: "brightness(0) invert(1)" }} />
+                    <IconButton onClick={()=> setAbierto(true)} sx={{ml: 1,height: "55px", width: "55px" , padding: "5px", objectFit: "cover", color: "white"}}
+                    ><MenuIcon></MenuIcon></IconButton>
+                    <Drawer open={abierto} anchor="left" onClose={()=> setAbierto(false)}><MenuNavbar></MenuNavbar> </Drawer>
+                    {/* <Button>
+                        <MenuIcon size="large" edge="start" color="orange" aria-label="menu" sx={{ mr: 2 }} ></MenuIcon>
+                    </Button> */}
+                        <img src="modex.png" alt="logo modex" style={{ width: "150px", height: "auto", padding: "15px", paddingRight: "10px" , filter: "brightness(0) invert(1)" }} />
                     <CustomizedInputBase ></CustomizedInputBase>
                     <Button variant="contained" size="large" startIcon={<PersonIcon/>} sx={{ml: 2, backgroundColor: "#a111ad", borderRadius: "20px", objectFit: "contain"}}>Ingresar</Button>
                     <IconButton variant="contained" sx={{ml: 2, backgroundColor: "#a111ad", borderRadius: 20, height: "45px", width: "45px" , padding: "5px", objectFit: "cover", color: "white"}}><FavoriteIcon color="inherit"/></IconButton>
