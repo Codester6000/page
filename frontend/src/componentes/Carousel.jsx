9f779d862118ecdf57c6d803938f36e921228669
Ramon Barrera <rebcontacto@gmail.com>
2024-11-18 22:11:28 -0300

    Se mejoro el diseño de las Cards para mostrar los productos

--- frontend/src/componentes/Carousel.jsx
+++ frontend/src/componentes/Carousel.jsx
@@ -6,10 +6,17 @@ import './Carousel.css'
 // poner en la terminar
 // npm install swiper
 
+// para tener mejor obtimizadas las imagenes del carusel
+// deben de ser imagenes en 1600 x350px
+// el contenido tiene que estar bien centrado así evitamos
+// que se pierda el contenido importante
 
-
+// tengo que hacer que tome las imagenes de una carpeta especifica
+//(probablemente "/public/noticias/" y dejar una cantidad especifica)
+// proximamente hacer que no tenga limitacion de cantidad
 
 const Carousel = () => {
+  
   return (
     <Swiper
       className='swiperR'
