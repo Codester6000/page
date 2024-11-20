586eeef72743eba8d6c53606da419f6a2c0943da
Salinas Matias <salinasmatiasit@gmail.com>
2024-11-19 23:01:30 -0300

    sql dimensiones modificado

--- backend/armador.js
+++ backend/armador.js
@@ -1,39 +1,28 @@
 import express from "express"
 import { validarQueryArmador } from "./middleware/validaciones.js";
-import { body,query,validationResult} from "express-validator";
-import {db} from "./database/connectionMySQL.js";
+import { body, query, validationResult } from "express-validator";
+import { db } from "./database/connectionMySQL.js";
 
 const armadorRouter = express.Router()
 
-armadorRouter.get("/",validarQueryArmador(),async(req,res) => {
+armadorRouter.get("/", validarQueryArmador(), async (req, res) => {
     const validacion = validationResult(req);
-  if (!validacion.isEmpty()) {
-    res.status(400).send({ errores: validacion.array() });
-    return;
-  }
+    if (!validacion.isEmpty()) {
+        res.status(400).send({ errores: validacion.array() });
+        return;
+    }
 
-//  const procesador = req.query.procesador;
-//     if (procesador != undefined){
-
-//     }
-//     const motherboard = req.query.motherboard;
-//     if (motherboard != undefined){
-
-//     }
-
-
-const [procesadores] = await db.execute(`SELECT pr.nombre, pr.stock, pr.peso, pr.garantia_meses, pr.codigo_fabricante,
+    const [procesadores] = await db.execute(`SELECT pr.nombre, pr.stock, pr.peso, pr.garantia_meses, pr.codigo_fabricante,
        GROUP_CONCAT(DISTINCT c.nombre_categoria SEPARATOR ', ') AS categorias,
        GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
        p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva,
-       d.alto, d.ancho, d.largo, pro.nombre_proveedor
+       pr.alto, pr.ancho, pr.largo, pro.nombre_proveedor
 FROM productos pr
 INNER JOIN precios p ON pr.id_producto = p.id_producto
 INNER JOIN productos_categorias pc ON pr.id_producto = pc.id_producto
 INNER JOIN categorias c ON pc.id_categoria = c.id_categoria
 INNER JOIN productos_imagenes pi ON pi.id_producto = pr.id_producto
 INNER JOIN imagenes i ON pi.id_imagen = i.id_imagen
-INNER JOIN dimensiones d ON d.id_producto = pr.id_producto
 INNER JOIN proveedores pro ON pro.id_proveedor = p.id_proveedor
 WHERE 
     p.precio_dolar = (
@@ -47,22 +36,21 @@ WHERE
         INNER JOIN categorias c2 ON pc2.id_categoria = c2.id_categoria
         WHERE c2.nombre_categoria = "procesadores"
     )
-GROUP BY pr.id_producto, p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva, d.alto, d.ancho, d.largo, pro.nombre_proveedor;
+GROUP BY pr.id_producto, p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva, pr.alto, pr.ancho, pr.largo, pro.nombre_proveedor;
 `)
 
 
-        const [motherboards] = await db.execute(`SELECT pr.nombre, pr.stock, pr.peso, pr.garantia_meses, pr.codigo_fabricante,
-       GROUP_CONCAT(DISTINCT c.nombre_categoria SEPARATOR ', ') AS categorias,
-       GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
-       p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva,
-       d.alto, d.ancho, d.largo, pro.nombre_proveedor
+    const [motherboards] = await db.execute(`SELECT pr.nombre, pr.stock, pr.peso, pr.garantia_meses, pr.codigo_fabricante,
+GROUP_CONCAT(DISTINCT c.nombre_categoria SEPARATOR ', ') AS categorias,
+GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
+p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva,
+pr.alto, pr.ancho, pr.largo, pro.nombre_proveedor
 FROM productos pr
 INNER JOIN precios p ON pr.id_producto = p.id_producto
 INNER JOIN productos_categorias pc ON pr.id_producto = pc.id_producto
 INNER JOIN categorias c ON pc.id_categoria = c.id_categoria
 INNER JOIN productos_imagenes pi ON pi.id_producto = pr.id_producto
 INNER JOIN imagenes i ON pi.id_imagen = i.id_imagen
-INNER JOIN dimensiones d ON d.id_producto = pr.id_producto
 INNER JOIN proveedores pro ON pro.id_proveedor = p.id_proveedor
 WHERE 
     p.precio_dolar = (
@@ -76,22 +64,21 @@ WHERE
         INNER JOIN categorias c2 ON pc2.id_categoria = c2.id_categoria
         WHERE c2.nombre_categoria = "motherboards"
     )
-GROUP BY pr.id_producto, p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva, d.alto, d.ancho, d.largo, pro.nombre_proveedor;
+GROUP BY pr.id_producto, p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva, pr.alto, pr.ancho, pr.largo, pro.nombre_proveedor;
 `)
 
 
-const [memorias] = await db.execute(`SELECT pr.nombre, pr.stock, pr.peso, pr.garantia_meses, pr.codigo_fabricante,
-       GROUP_CONCAT(DISTINCT c.nombre_categoria SEPARATOR ', ') AS categorias,
-       GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
-       p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva,
-       d.alto, d.ancho, d.largo, pro.nombre_proveedor
+    const [memorias] = await db.execute(`SELECT pr.nombre, pr.stock, pr.peso, pr.garantia_meses, pr.codigo_fabricante,
+GROUP_CONCAT(DISTINCT c.nombre_categoria SEPARATOR ', ') AS categorias,
+GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
+p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva,
+pr.alto, pr.ancho, pr.largo, pro.nombre_proveedor
 FROM productos pr
 INNER JOIN precios p ON pr.id_producto = p.id_producto
 INNER JOIN productos_categorias pc ON pr.id_producto = pc.id_producto
 INNER JOIN categorias c ON pc.id_categoria = c.id_categoria
 INNER JOIN productos_imagenes pi ON pi.id_producto = pr.id_producto
 INNER JOIN imagenes i ON pi.id_imagen = i.id_imagen
-INNER JOIN dimensiones d ON d.id_producto = pr.id_producto
 INNER JOIN proveedores pro ON pro.id_proveedor = p.id_proveedor
 WHERE 
     p.precio_dolar = (
@@ -105,79 +92,76 @@ WHERE
         INNER JOIN categorias c2 ON pc2.id_categoria = c2.id_categoria
         WHERE c2.nombre_categoria = "memorias pc"
     )
-GROUP BY pr.id_producto, p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva, d.alto, d.ancho, d.largo, pro.nombre_proveedor;
+GROUP BY pr.id_producto, p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva, pr.alto, pr.ancho, pr.largo, pro.nombre_proveedor;
 `)
 
 
-        const [fuentes] = await db.execute(`SELECT pr.nombre,pr.stock,pr.peso,pr.garantia_meses,pr.codigo_fabricante,GROUP_CONCAT(c.nombre_categoria SEPARATOR ', ') AS categorias
-            ,GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
-            p.precio_dolar, p.precio_dolar_iva, p.iva,p.precio_pesos, p.precio_pesos_iva,
-            d.alto,d.ancho,d.largo,pro.nombre_proveedor
-            FROM productos pr 
-            INNER JOIN precios p 
-            ON pr.id_producto = p.id_producto 
-            INNER JOIN productos_categorias pc 
-            ON pr.id_producto = pc.id_producto 
-            INNER JOIN categorias c ON pc.id_categoria = c.id_categoria
-            INNER JOIN productos_imagenes pi ON pi.id_producto = pr.id_producto
-            INNER JOIN imagenes i ON pi.id_imagen = i.id_imagen
-            INNER JOIN dimensiones d ON d.id_producto = pr.id_producto
-            INNER JOIN proveedores pro ON pro.id_proveedor = p.id_proveedor
-            WHERE 
-                p.precio_dolar = (
+    const [fuentes] = await db.execute(`SELECT pr.nombre,pr.stock,pr.peso,pr.garantia_meses,pr.codigo_fabricante,GROUP_CONCAT(c.nombre_categoria SEPARATOR ', ') AS categorias,
+    GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
+    p.precio_dolar, p.precio_dolar_iva, p.iva,p.precio_pesos, p.precio_pesos_iva,
+    pr.alto,pr.ancho,pr.largo,pro.nombre_proveedor
+    FROM productos pr 
+    INNER JOIN precios p 
+    ON pr.id_producto = p.id_producto 
+    INNER JOIN productos_categorias pc 
+    ON pr.id_producto = pc.id_producto 
+    INNER JOIN categorias c ON pc.id_categoria = c.id_categoria
+    INNER JOIN productos_imagenes pi ON pi.id_producto = pr.id_producto
+    INNER JOIN imagenes i ON pi.id_imagen = i.id_imagen
+    INNER JOIN proveedores pro ON pro.id_proveedor = p.id_proveedor
+    WHERE 
+        p.precio_dolar = (
                     SELECT MIN(precio_dolar) 
                     FROM precios 
                     WHERE id_producto = pr.id_producto
                     ) AND c.nombre_categoria = "fuentes"
-                    group by pr.id_producto, p.precio_dolar, p.precio_dolar_iva,p.iva,p.precio_pesos, p.precio_pesos_iva,d.alto,d.ancho,d.largo,pro.nombre_proveedor;`)
+                    group by pr.id_producto, p.precio_dolar, p.precio_dolar_iva,p.iva,p.precio_pesos, p.precio_pesos_iva,pr.alto,pr.ancho,pr.largo,pro.nombre_proveedor;`)
 
-                    const [gabinetes] = await db.execute(`SELECT pr.nombre,pr.stock,pr.peso,pr.garantia_meses,pr.codigo_fabricante,GROUP_CONCAT(c.nombre_categoria SEPARATOR ', ') AS categorias
-                        ,GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
-                        p.precio_dolar, p.precio_dolar_iva, p.iva,p.precio_pesos, p.precio_pesos_iva,
-                        d.alto,d.ancho,d.largo,pro.nombre_proveedor
-                        FROM productos pr 
-                        INNER JOIN precios p 
-                        ON pr.id_producto = p.id_producto 
-                        INNER JOIN productos_categorias pc 
-                        ON pr.id_producto = pc.id_producto 
-                        INNER JOIN categorias c ON pc.id_categoria = c.id_categoria
-                        INNER JOIN productos_imagenes pi ON pi.id_producto = pr.id_producto
-                        INNER JOIN imagenes i ON pi.id_imagen = i.id_imagen
-                        INNER JOIN dimensiones d ON d.id_producto = pr.id_producto
-                        INNER JOIN proveedores pro ON pro.id_proveedor = p.id_proveedor
-                        WHERE 
-                            p.precio_dolar = (
-                                SELECT MIN(precio_dolar) 
-                                FROM precios 
-                                WHERE id_producto = pr.id_producto
-                                ) AND c.nombre_categoria = "gabinetes"
-                                group by pr.id_producto, p.precio_dolar, p.precio_dolar_iva,p.iva,p.precio_pesos, p.precio_pesos_iva,d.alto,d.ancho,d.largo,pro.nombre_proveedor;`)
-                                const [almacenamiento] = await db.execute(`
-                                    SELECT pr.nombre, pr.stock, pr.peso, pr.garantia_meses, pr.codigo_fabricante,
-                                           GROUP_CONCAT(c.nombre_categoria SEPARATOR ', ') AS categorias,
-                                           GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
-                                           p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva,
-                                           d.alto, d.ancho, d.largo, pro.nombre_proveedor
-                                    FROM productos pr
-                                    INNER JOIN precios p ON pr.id_producto = p.id_producto
-                                    INNER JOIN productos_categorias pc ON pr.id_producto = pc.id_producto
-                                    INNER JOIN categorias c ON pc.id_categoria = c.id_categoria
-                                    INNER JOIN productos_imagenes pi ON pi.id_producto = pr.id_producto
-                                    INNER JOIN imagenes i ON pi.id_imagen = i.id_imagen
-                                    INNER JOIN dimensiones d ON d.id_producto = pr.id_producto
-                                    INNER JOIN proveedores pro ON pro.id_proveedor = p.id_proveedor
-                                    WHERE 
-                                        p.precio_dolar = (
-                                            SELECT MIN(precio_dolar)
-                                            FROM precios
-                                            WHERE id_producto = pr.id_producto
-                                        )
-                                        AND c.nombre_categoria IN ("discos internos", "discos internos ssd")
-                                    GROUP BY pr.id_producto, p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva, d.alto, d.ancho, d.largo, pro.nombre_proveedor;
+    const [gabinetes] = await db.execute(`SELECT pr.nombre,pr.stock,pr.peso,pr.garantia_meses,pr.codigo_fabricante,GROUP_CONCAT(c.nombre_categoria SEPARATOR ', ') AS categorias
+        ,GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
+        p.precio_dolar, p.precio_dolar_iva, p.iva,p.precio_pesos, p.precio_pesos_iva,
+        pr.alto,pr.ancho,pr.largo,pro.nombre_proveedor
+        FROM productos pr 
+        INNER JOIN precios p 
+        ON pr.id_producto = p.id_producto 
+        INNER JOIN productos_categorias pc 
+        ON pr.id_producto = pc.id_producto 
+        INNER JOIN categorias c ON pc.id_categoria = c.id_categoria
+        INNER JOIN productos_imagenes pi ON pi.id_producto = pr.id_producto
+        INNER JOIN imagenes i ON pi.id_imagen = i.id_imagen
+        INNER JOIN proveedores pro ON pro.id_proveedor = p.id_proveedor
+        WHERE 
+            p.precio_dolar = (
+                SELECT MIN(precio_dolar) 
+                FROM precios 
+                WHERE id_producto = pr.id_producto
+                ) AND c.nombre_categoria = "gabinetes"
+                group by pr.id_producto, p.precio_dolar, p.precio_dolar_iva,p.iva,p.precio_pesos, p.precio_pesos_iva,pr.alto,pr.ancho,pr.largo,pro.nombre_proveedor;`)
+    const [almacenamiento] = await db.execute(`
+        SELECT pr.nombre, pr.stock, pr.peso, pr.garantia_meses, pr.codigo_fabricante,
+        GROUP_CONCAT(c.nombre_categoria SEPARATOR ', ') AS categorias,
+        GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
+        p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva,
+        pr.alto, pr.ancho, pr.largo, pro.nombre_proveedor
+        FROM productos pr
+        INNER JOIN precios p ON pr.id_producto = p.id_producto
+        INNER JOIN productos_categorias pc ON pr.id_producto = pc.id_producto
+        INNER JOIN categorias c ON pc.id_categoria = c.id_categoria
+        INNER JOIN productos_imagenes pi ON pi.id_producto = pr.id_producto
+        INNER JOIN imagenes i ON pi.id_imagen = i.id_imagen
+        INNER JOIN proveedores pro ON pro.id_proveedor = p.id_proveedor
+        WHERE 
+            p.precio_dolar = (
+                SELECT MIN(precio_dolar)
+                FROM precios
+                WHERE id_producto = pr.id_producto
+            )
+            AND c.nombre_categoria IN ("discos internos", "discos internos ssd")
+        GROUP BY pr.id_producto, p.precio_dolar, p.precio_dolar_iva, p.iva, p.precio_pesos, p.precio_pesos_iva, pr.alto, pr.ancho, pr.largo, pro.nombre_proveedor;
                                 `);
-                                
-    
-res.status(200).send({procesadores: procesadores,motherboards:motherboards,memorias:memorias,fuentes:fuentes,gabinetes:gabinetes,almacenamiento:almacenamiento})
+
+
+    res.status(200).send({ procesadores: procesadores, motherboards: motherboards, memorias: memorias, fuentes: fuentes, gabinetes: gabinetes, almacenamiento: almacenamiento })
 })
 
 export default armadorRouter;
--- backend/productos.js
+++ backend/productos.js
@@ -14,7 +14,7 @@ productosRouter.get("/",validarQuerysProducto(),async (req, res) => {
     let sql = `SELECT pr.nombre,pr.stock,pr.peso,pr.garantia_meses,pr.codigo_fabricante,GROUP_CONCAT(c.nombre_categoria SEPARATOR ', ') AS categorias
 ,GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
 p.precio_dolar, p.precio_dolar_iva, p.iva,p.precio_pesos, p.precio_pesos_iva,
-d.alto,d.ancho,d.largo,pro.nombre_proveedor
+pr.alto,pr.ancho,pr.largo,pro.nombre_proveedor
 FROM productos pr 
 INNER JOIN precios p 
 ON pr.id_producto = p.id_producto 
@@ -23,7 +23,6 @@ ON pr.id_producto = pc.id_producto
 INNER JOIN categorias c ON pc.id_categoria = c.id_categoria
 INNER JOIN productos_imagenes pi ON pi.id_producto = pr.id_producto
 INNER JOIN imagenes i ON pi.id_imagen = i.id_imagen
-INNER JOIN dimensiones d ON d.id_producto = pr.id_producto
 INNER JOIN proveedores pro ON pro.id_proveedor = p.id_proveedor
 WHERE 
     p.precio_dolar = (
@@ -34,7 +33,7 @@ WHERE
 
     
 
-let sqlParteFinal = ` group by pr.id_producto, p.precio_dolar, p.precio_dolar_iva,p.iva,p.precio_pesos, p.precio_pesos_iva,d.alto,d.ancho,d.largo,pro.nombre_proveedor;`
+let sqlParteFinal = ` group by pr.id_producto, p.precio_dolar, p.precio_dolar_iva,p.iva,p.precio_pesos, p.precio_pesos_iva,pr.alto,pr.ancho,pr.largo,pro.nombre_proveedor;`
     const filtros = []
     const parametros = []
 
@@ -75,7 +74,7 @@ productosRouter.get("/:id", async (req, res) => {
     const [resultado, fields] = await db.execute(`SELECT pr.nombre,pr.stock,pr.peso,pr.garantia_meses,pr.codigo_fabricante,GROUP_CONCAT(c.nombre_categoria SEPARATOR ', ') AS categorias
 ,GROUP_CONCAT(DISTINCT i.url_imagen SEPARATOR ', ') AS url_imagenes,
 p.precio_dolar, p.precio_dolar_iva, p.iva,p.precio_pesos, p.precio_pesos_iva,
-d.alto,d.ancho,d.largo,pro.nombre_proveedor
+pr.alto,pr.ancho,pr.largo,pro.nombre_proveedor
 FROM productos pr 
 INNER JOIN precios p 
 ON pr.id_producto = p.id_producto 
@@ -84,7 +83,6 @@ ON pr.id_producto = pc.id_producto
 INNER JOIN categorias c ON pc.id_categoria = c.id_categoria
 INNER JOIN productos_imagenes pi ON pi.id_producto = pr.id_producto
 INNER JOIN imagenes i ON pi.id_imagen = i.id_imagen
-INNER JOIN dimensiones d ON d.id_producto = pr.id_producto
 INNER JOIN proveedores pro ON pro.id_proveedor = p.id_proveedor
 WHERE 
     p.precio_dolar = (
@@ -92,7 +90,7 @@ WHERE
         FROM precios 
         WHERE id_producto = pr.id_producto
         ) AND pr.id_producto = ?
-        group by pr.id_producto, p.precio_dolar, p.precio_dolar_iva,p.iva,p.precio_pesos, p.precio_pesos_iva,d.alto,d.ancho,d.largo,pro.nombre_proveedor;`,[id])
+        group by pr.id_producto, p.precio_dolar, p.precio_dolar_iva,p.iva,p.precio_pesos, p.precio_pesos_iva,pr.alto,pr.ancho,pr.largo,pro.nombre_proveedor;`,[id])
         
 res.status(200).send({datos: resultado})
 })
