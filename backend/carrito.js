c8deec92d837e76a8ae2392929681b488e690b10
Matias Salinas <salinasmatiasit@gmail.com>
2024-11-25 18:39:32 -0300

    carrito DELETE listo

--- backend/carrito.js
+++ backend/carrito.js
@@ -21,12 +21,19 @@ carritoRouter.post('/',validarJwt,validarBodyCarrito(),verificarValidaciones, as
     res.status(201).send({resultado:resul})
 })
 
-carritoRouter.delete('/:id',validarJwt,validarId,validarBodyCarrito(),async (req, res) => {
+carritoRouter.delete('/',validarJwt,validarId,validarBodyCarrito(),async (req, res) => {
     const {id_producto } = req.body;
-    const parametros = [req.user.userId,id_producto];
-    const sql = 'INSERT INTO carritos (id_usuario,id_producto) VALUES (?,?);';
-    console.log(parametros)
-    const [resul, fields] = await db.execute(sql,parametros)
-    res.status(201).send({resultado:resul})
+    try {
+        const parametros = [id_producto,req.user.userId];
+        const sql = 'DELETE FROM carritos WHERE id_producto = ? and id_usuario = ?';
+        const [resultado, fields] = await db.execute(sql,parametros)
+        if(resultado.affectedRows == 0) {
+            return res.status(404).send({mensaje:"Producto no encontrado en el carrito"})
+        }
+        res.status(200).send({mensaje:"Producto eliminado del carrito"})
+    } catch(error){
+        console.log(error)
+        res.status(500).send({error:"Error al eliminar el producto"})
+    }
 })
 export default carritoRouter;
