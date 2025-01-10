904866987bbe02341abaf5a66e933adca9bd3307
Salinas Matias <salinasmatiasit@gmail.com>
2025-01-09 21:07:23 -0300

    config prod y cors

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ let corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 };
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use("/auth",authRouter)
 
