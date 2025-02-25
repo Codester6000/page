eee75d172aedebd5a42de082398dc0335cfe830c
Salinas Matias <salinasmatiasit@gmail.com>
2025-02-25 05:21:56 -0300

    Carousel de productos

--- frontend/src/componentes/Portada.jsx
+++ frontend/src/componentes/Portada.jsx
@@ -1,5 +1,7 @@
 import '../styles/portada.css';
 import Carousel from './Carousel';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 export default function Portada(){
     return(
         <div>
@@ -17,7 +19,19 @@ export default function Portada(){
                     <h1>NUEVOS INGRESOS</h1>
                 <div className="lineaNaranja"></div>
                 </div>
-                <div className="productosPortada"></div>
+                <div className="productosPortada">
+                <Swiper watchSlidesProgress={true} slidesPerView={3} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+                </div>
             </div>
             <div className="armados">
                 <h2 className="armadosTitulo">ARMADOS</h2>
