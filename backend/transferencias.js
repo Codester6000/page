ba29ce5d067443a8ff339c857e6e55bd7d1382ac
Salinas Matias <salinasmatiasit@gmail.com>
2025-05-18 04:14:51 -0300

    funciones para actualizar carrito con archivo de transferencia

--- backend/transferencias.js
+++ backend/transferencias.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { validarJwt } from "./auth.js";
 import multer from "multer"
 import path from 'path'
+import { db } from './database/connectionMySQL.js';
 
 const transferenciasRouter = express.Router();
 const imgconfig = multer.diskStorage({
@@ -27,12 +28,25 @@ const upload = multer({
     }
 
  })
+
+const transferenciaApendiente = async (id, total_a_pagar) => {
+    const sql = "UPDATE carrito SET estado = 'Pendiente', fecha_finalizada = CURRENT_TIMESTAMP(), total_a_pagar = ? WHERE id_carrito = ? ;";
+    const [response] = await db.execute(sql,[total_a_pagar,id]);
+    return response?.affectedRows > 0;
+
+}
+const obtenerTotalCarrito = async(id_productos) => {
+    const sql = "SELECT SUM(p.precio_pesos_iva) FROM precios p INNER JOIN carrito_detalle cd ON p.id_producto = cd.id_producto WHERE id_carrito = ?; "
+}
 transferenciasRouter.post('/single', validarJwt, upload.single("imagenTransferencia"), async (req, res) => {
     try {
         if (!req.file) {
             return res.status(400).json({ error: 'No file uploaded' });
         }
         console.log(req.file);
+        const {id_carrito, id_productos} = req.body;
+        
+        
         res.send("ok");
     } catch (error) {
         res.status(500).json({ error: error.message });
