e806382f6713d95fde4eaa7ec8c46bb7ca81379e
Lucas Falcon <tutuca6000@gmail.com>
2025-11-03 20:33:21 -0300

    actualizacion backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -46,7 +46,8 @@ const inicializarServidor = async () => {
         const allowedOrigins = [
           "https://modex.com.ar",
           "https://www.modex.com.ar",
-          "http://localhost:5173",
+          "http://localhost:5173", 
+          "http://66.97.43.219:5173",
         ];
         if (allowedOrigins.includes(origin) || !origin) {
           callback(null, true);
